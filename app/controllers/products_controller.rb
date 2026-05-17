f503f80fb602b166672d8dc5c3d8b5981d4dcea7
Isaac Machado <contato@isaacmachado.com.br>
2026-05-17 12:28:30 -0300

    feat: active storage to attach images, login/logout, translation...

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,8 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show edit update destroy
-
+  allow_unauthenticated_access only: %i[ index show
 ]
-
-
+  before_action :set_product, only: %i[ show edit update destroy
+]
 
   def index
     @products = Product.all
@@ -15,8 +14,6 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
-
-
 
   def create
     @product = Product.new(product_params)
@@ -49,7 +46,11 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.expect(product: [ :name ] )
+      params.expect(product: [ 
+          :name, 
+          :description,
+          :featured_image
+        ])
     end
 
 end
